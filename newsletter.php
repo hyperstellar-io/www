3f5758bed7aa9ad1a88a4c39dbcabfa4f684eeaa
cmulay <codewithchin@gmail.com>
2023-06-04 19:41:31 +0530

    Implemented backend

--- newsletter.php
+++ newsletter.php
@@ -1,67 +1,43 @@
 <?php
-if(isset($_POST['email'])) {
- 
-    // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "contact@engitect.com";
-    $email_subject = "Your email subject line";
- 
-    function died($error) {
-        // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
-        echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
-    }
- 
- 
-    // validation expected data exists
-    if(!isset($_POST['email'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
-    }
- 
-     
- 
-    $email_from = $_POST['email']; // required
- 
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
- 
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
- 
-    $email_message = "Form details below.\n\n";
- 
-     
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
- 
-     
- 
-    $email_message .= "Email: ".clean_string($email_from)."\n";
- 
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);  
-?>
- 
-<!-- include your own success html here -->
- 
-Thank you for subcribe us.
- 
-<?php
- 
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+require 'plugins/phpmailer/src/Exception.php';
+require 'plugins/phpmailer/src/PHPmailer.php';
+require 'plugins/phpmailer/src/SMTP.php';
+require 'environment.php';
+
+$usrEmail = $_POST['email'];
+
+$email_subject = "New Newsletter joinee.";
+$email_body = "Email: " . $usrEmail . " is subscribed to newsletter";
+
+if (isset($_POST["send"])) {
+  $mail = new PHPMailer(true);
+
+  $mail->isSMTP();
+  $mail->Host = "smtp.gmail.com";
+  $mail->SMTPAuth = true;
+  $mail->Username = $_ENV["SYS_EMAIL"]; //our Gmail
+  $mail->Password = $_ENV["SYS_PASSWORD"]; // Gmail App Psswords
+  $mail->SMTPSecure = 'ssl';
+  $mail->Port = 465;
+
+  $mail->setFrom($usrEmail);
+
+  $mail->addAddress($_ENV["SYS_EMAIL"], "noreply"); //Our Gmail
+
+  $mail->isHTML(true);
+
+  $mail->Subject = $email_subject;
+  $mail->Body = $email_body;
+
+  $mail->send();
+
+  echo "
+    <script>
+    alert('We have added you to our mailing list');
+    document.location.href = 'cheers.html';
+    </script>
+    ";
 }
-?>
